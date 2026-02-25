5886fc97779fbfbb73f90a36953892f6758afba1
Yhtomitos <67652876+Yhtomitos@users.noreply.github.com>
2026-02-25 15:09:19 -0800

    Create and draw midline structure upon window opening

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Export.hpp>
+#include <SFML/Window/Event.hpp>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
@@ -38,6 +41,21 @@ struct Paddle
     sf::FloatRect bounds() const { return shape.getGlobalBounds(); }
 };
 
+struct MidLine
+{
+	std::vector<sf::RectangleShape> segments;
+
+	// Create a dashed midline with the specified number of dashes
+    MidLine(int numDashes) {
+        for (int i = 0; i < numDashes; ++i) {
+            sf::RectangleShape dash({ 4.f, 20.f });
+            dash.setFillColor(sf::Color::White);
+            dash.setPosition({ WINDOW_W / 2.f - 2.f, i * (WINDOW_H / numDashes) + 10.f });
+			segments.push_back(dash);
+        }
+    }
+};
+
 
 int main() {
 
@@ -48,6 +66,9 @@ int main() {
 	// Create Paddles for player and AI
     Paddle playerPaddle(40.f);
     Paddle aiPaddle(WINDOW_W - 40.f);
+
+	// Create Midline
+	MidLine midline(15);
 
 	// Game Loop
 	while ( window.isOpen() )
@@ -62,6 +83,9 @@ int main() {
 		// Draw Paddles
         window.draw(playerPaddle.shape);
         window.draw(aiPaddle.shape);
+
+		// Draw Midline
+        for (const auto& dash : midline.segments) {window.draw(dash);}
 
 		// Opens Window
 		window.display();
